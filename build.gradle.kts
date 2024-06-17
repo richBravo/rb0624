d3f1da1cd0a138c057b25b4ae7d292b19ed281fe
richBravo <richard.alexander.bravo@hotmail.com>
2024-06-16 23:32:54 -0500

    feature: add initial project structure

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-core:5.12.0")
+    testImplementation("junit:junit:4.12")
+    annotationProcessor("org.projectlombok:lombok:1.18.32")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
